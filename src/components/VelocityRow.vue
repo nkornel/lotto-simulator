42854454017e5dd0e3796648b24810374b42332d
Kornel <kornel.novak@webcapital.hu>
2023-06-24 17:30:45 +0200

    Slider styling

--- src/components/VelocityRow.vue
+++ src/components/VelocityRow.vue
@@ -2,12 +2,31 @@
     <div class="container flex flex-col">
         <label class="mb-4">Speed</label>
         <div class="w-full">
-            <input type="range" min="1" max="100" value="50" class="slider w-full appearance-none h-2 bg-lotto-gray border-md" id="myRange">
+            <div class="h-1 w-full bg-lotto-gray relative">
+                <input ref="velocityRange" v-on:input="handleChange" type="range" min="1" max="100" v-model="velocity" class="absolute slider w-full appearance-none h-2 bg-lotto-gray -top-0.5">
+            </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
+import {ref} from 'vue';
+const velocityRange = ref();
+const velocity = ref(1);
+
+function handleChange(e) {
+    console.log(velocity);
+    let target = e.target
+    if (e.target.type !== 'range') {
+        target = document.getElementById('range')
+    } 
+    const min = target.min
+    const max = target.max
+    const val = target.value
+    
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
 </script>
 
 <style>
@@ -15,22 +34,21 @@
 input[type="range"] {
   -webkit-appearance: none;
   margin-right: 15px;
-  width: 200px;
-  height: 7px;
-  background: green;
-  border-radius: 5px;
-  background-image: linear-gradient(#ff4500, #ff4500);
-  background-size: 70% 100%;
+  height: 8px;
+  background: transparent;
+  border-radius: 8px;
+  background-image: linear-gradient(#A5D9C8, #A5D9C8);
+  background-size: 1% 100%;
   background-repeat: no-repeat;
 }
 
 /* Input Thumb */
 input[type="range"]::-webkit-slider-thumb {
   -webkit-appearance: none;
-  height: 20px;
-  width: 20px;
+  height: 8px;
+  width: 8px;
   border-radius: 50%;
-  background: #ff4500;
+  background: #fff;
   cursor: ew-resize;
   box-shadow: 0 0 2px 0 #555;
   transition: background .3s ease-in-out;
@@ -38,10 +56,10 @@ input[type="range"]::-webkit-slider-thumb {
 
 input[type="range"]::-moz-range-thumb {
   -webkit-appearance: none;
-  height: 20px;
-  width: 20px;
+  height: 8px;
+  width: 8px;
   border-radius: 50%;
-  background: #ff4500;
+  background: #fff;
   cursor: ew-resize;
   box-shadow: 0 0 2px 0 #555;
   transition: background .3s ease-in-out;
@@ -49,25 +67,25 @@ input[type="range"]::-moz-range-thumb {
 
 input[type="range"]::-ms-thumb {
   -webkit-appearance: none;
-  height: 20px;
-  width: 20px;
+  height: 8px;
+  width: 8px;
   border-radius: 50%;
-  background: #ff4500;
+  background: #fff;
   cursor: ew-resize;
   box-shadow: 0 0 2px 0 #555;
   transition: background .3s ease-in-out;
 }
 
 input[type="range"]::-webkit-slider-thumb:hover {
-  background: #ff0200;
+  background: #fff;
 }
 
 input[type="range"]::-moz-range-thumb:hover {
-  background: #ff0200;
+  background: #fff;
 }
 
 input[type="range"]::-ms-thumb:hover {
-  background: #ff0200;
+  background: #fff;
 }
 
 /* Input Track */
